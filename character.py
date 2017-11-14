c6551c33ff076958aae50c3470ca25caeec14e48
Roy Healy <roy.healy87+gh@gmail.com>
2017-11-14 23:54:38 +0000

    Add files via upload
    Adding natural language command version of Dicecord

--- character.py
+++ character.py
@@ -1,69 +1,64 @@
-#Character object
+# Character object
 
 import random
 
+
 class Character:
-    def __init__(self, ID):
-        '''
-        Class for holding players, making their rolls and saving their last rolls
-        '''
-        #Some means of identifying user
-        #Generally either numeric ID or discord username depending on bot versus client implementation
+    def __init__(self, ID, splat='default', flavour=True):
+        """Class for holding players, making their rolls and saving their last rolls"""
+
         self.ID = ID
         self.rolls = []
-        self.splat = ''
-        self.flavour = True
-        
-        self.goodMessages = ["You should take the beat, [userID].",
-                    "Aren't I a good bot, [userID]?",
-                    "Did you hack me, [userID]?",
-                    "Masterfully done, [userID]!",
-                    "Don't let this luck go to waste, [userID]."]
-        self.badMessages = ["Don't blame your bad luck on me, [userID]! I'm just a random number generator.",
-                   "That was just a practice roll, right [userID]?",
-                   "[userID] rolls like a diary farmer.",
-                   "Ask for a dramatic failure [userID], you know you want to!",
-                   "[userID], I hope that wasn't an important roll ..."]
+        self.changeSplat(splat)
+        self.flavour = flavour
 
     def changeSplat(self, splat):
         if splat == 'mage':
             self.splat = 'mage'
             self.goodMessages = ["Masterfully done, [userID]!",
-                    "Don't let this luck go to waste, [userID].",
-                    "The Lie cannot withstand your will, [userID]!",
-                    "Reality is yours to command, [userID]!",
-                    "You should take the beat, [userID].",
-                    "Aren't I a good bot, [userID]?",
-                    "[userID] is a conduit to the supernal!",
-                    "Did you hack me, [userID]?",
-                    "[userID], if you were still a sleeper the majesty of this action would have awoken you!"]
+                                 "Don't let this luck go to waste, [userID].",
+                                 "The Lie cannot withstand your will, [userID]!",
+                                 "Reality is yours to command, [userID]!",
+                                 "You should take the beat, [userID].",
+                                 "Aren't I a good bot, [userID]?",
+                                 "[userID] is a conduit to the supernal!",
+                                 "Did you hack me, [userID]?",
+                                 "[userID], if you were still a sleeper the majesty of this action would have awoken you!"]
 
             self.badMessages = ["[userID]'s nimbus looks like a wet dishrag.",
-                   "The lie constricts your potential, [userID].",
-                   "Don't blame your bad luck on me, [userID]! I'm just a random number generator.",
-                   "That was just a practice roll, right [userID]?",
-                   "[userID] rolls like a diary farmer.",
-                   "Ask for a dramatic failure [userID], you know you want to!",
-                   "[userID], I hope that wasn't an important roll ...",
-                   "[userID]'s watchtower called out to the wrong soul."]
+                                "The lie constricts your potential, [userID].",
+                                "Don't blame your bad luck on me, [userID]! I'm just a random number generator.",
+                                "That was just a practice roll, right [userID]?",
+                                "[userID] rolls like a diary farmer.",
+                                "Ask for a dramatic failure [userID], you know you want to!",
+                                "[userID], I hope that wasn't an important roll ...",
+                                "[userID]'s watchtower called out to the wrong soul."]
 
             return "Splat set to Mage in "
 
-        elif splat == '':
-            return "No splat entered, please retry."
+        elif splat == 'default':
+            self.splat = 'default'
+            self.goodMessages = ["You should take the beat, [userID].",
+                                 "Aren't I a good bot, [userID]?",
+                                 "Did you hack me, [userID]?",
+                                 "Masterfully done, [userID]!",
+                                 "Don't let this luck go to waste, [userID]."]
+            self.badMessages = ["Don't blame your bad luck on me, [userID]! I'm just a random number generator.",
+                                "That was just a practice roll, right [userID]?",
+                                "[userID] rolls like a diary farmer.",
+                                "Ask for a dramatic failure [userID], you know you want to!",
+                                "[userID], I hope that wasn't an important roll ..."]
         else:
             return "No custom settings for " + splat + ". Messaging unchanged in "
 
-    def roll_set(self, dice, rote=False, again=10, quiet=False):
-        '''
-        roll a hand of dice subject to supplied conditions
+    def roll_set(self, dice, rote=False, again=10, quiet=True):
+        """roll a hand of dice subject to supplied conditions
         dice: int, the number of dice to roll
         rote: boolean, a rote roll rerolls all failed dice once
         again: int, which die faces explode
         quiet: Boolean about whether quiet mode will be used
         Returns a list of strings stating each die result and then total successes
-        If quiet mode, returns a one element list that displays returns total successes and result of each die.
-        '''
+        If quiet mode, returns a one element list that displays returns total successes and result of each die."""
         
         # Check that more than 1 die selected
         if dice < 1:
@@ -79,8 +74,7 @@ class Character:
         # fail collector in case it is a rote
         fails = []
 
-        
-        for die in range(0,dice):
+        for die in range(0, dice):
             # roll each die
             result = self.roll_die(again)
             if result == 0:
@@ -93,7 +87,7 @@ class Character:
         if rote:
             # if a rote all failed dice are rerolled once
             for die in fails:
-                successes += self.roll_die(again, rote_reroll = True)
+                successes += self.roll_die(again, rote_reroll=True)
 
         # send message
         messages = []
@@ -116,7 +110,6 @@ class Character:
                     out += " " + value
 
             messages.append(out)
-                
 
         # add total results message
         if not quiet:
@@ -135,11 +128,7 @@ class Character:
         return messages
         
     def bot_message(self, messagetype):
-        '''
-        Randomly sends a positive/negative message with very good or very bad rolls
-        :param type: "good" or "bad"
-        :return: none
-        '''
+        """Sends a random positive/negative message with very good or very bad rolls"""
         if messagetype == 'good':
                 out = random.choice(self.goodMessages)
         else:
@@ -147,15 +136,13 @@ class Character:
 
         return out.replace("[userID]", self.ID)
             
-    def roll_die(self, again = 10, explode_reroll = False, rote_reroll = False):
-        '''
-        Rolls a single die, calculates number of successes
+    def roll_die(self, again=10, explode_reroll=False, rote_reroll=False):
+        """Rolls a single die, calculates number of successes
         Also updates the last_roll attribute with each roll result
-        Handles explosions and gives custom last_roll text for rote/explosions
-        '''
+        Handles explosions and gives custom last_roll text for rote/explosions"""
+
         value = random.randrange(1, 11)
 
-        #recording value, adds details for rote/reroll
         if explode_reroll and rote_reroll:
             self.rolls.append(self.ID + " rolled rote exploded die: " + str(value))
         elif explode_reroll:
@@ -165,9 +152,9 @@ class Character:
         else:
             self.rolls.append(self.ID + " rolled " + str(value))
 
-        #checks for success/explosions
+        # Checks for success/explosions
         if value >= again:
-            #Exploding!
+            # Exploding!
             return 1 + self.roll_die(again, True, rote_reroll)
         elif value >= 8:
             return 1
@@ -175,23 +162,16 @@ class Character:
             return 0
     
     def roll_special(self):
-        '''
-        Rolls a single die, successes are not counted and last_roll not updated
-        '''
+        """Rolls a single die, successes are not counted and last_roll not updated"""
         value = random.randrange(1, 11)
         return self.ID + " rolled a " + str(value) + "!"
 
     def roll_chance(self):
-        '''
-        Rolls a chance die.
-        '''
-        #make roll by choosing random bumber between 1 and 10
+        """Rolls a chance die."""
         value = random.randrange(1, 11)
-
-        #Give value
         messages = [self.ID + "chance rolled " + str(value)]
 
-        ##check if failure, botch or success
+        # Check if failure, botch or success
         if value == 10:
             messages.append(self.ID + " got a success!")
             if self.flavour:
@@ -205,5 +185,4 @@ class Character:
             if self.flavour:
                 messages.append(self.bot_message("bad"))
 
-        #Give result
         return messages
